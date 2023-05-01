bf75d104cbd86b9caef0356a778f484178d44c65
pratikbagdi <96934729+pratikbagdi@users.noreply.github.com>
2023-05-02 00:08:49 +0530

    Add files via upload

---
+++ Task2/quiz.js
@@ -0,0 +1,134 @@
+let quiz_rules = [
+    "Solve the question within the limit of 10 sec",
+    "Click the button, to start the quiz.",
+];
+let quiz_questions = [
+    {
+        question: " Grand Central Terminal, Park Avenue, New York is the world's",
+
+        options : [
+            " largest railway station",
+            " highest railway station",
+            " longest railway station",
+            " None of the above",
+        ],
+        answer: 1,
+    },
+    
+    {
+        question: " Entomology is the science that studies",
+    
+        options: [
+            "Behavior of human beings",
+            "Insects",
+            "The origin and history of technical and scientific terms",
+            "The formation of rocks",
+        ],
+        answer: 2,
+    },
+    {
+        question: " Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
+    
+        options: [
+            "Asia",
+            "Africa",
+            "Europe",
+            "Australia",
+        ],
+        answer: 2,
+
+    },
+    {
+        question: " Garampani sanctuary is located at",
+    
+        options: [
+            "Junagarh, Gujarat",
+            "Diphu, Assam",
+            "Kohima, Nagaland",
+            "Gangtok, Sikkim",
+        ],
+        answer: 2,
+    },
+    {
+        question: " For which of the following disciplines is Nobel Prize awarded?",
+    
+        options: [
+            "Physics and Chemistry",
+            "Physiology or Medicine",
+            "Literature, Peace and Economics",
+            "All of the above",
+        ],
+        answer: 4,
+    },
+    {
+        question: " Hitler party which came into power in 1933 is known as",
+    
+        options: [
+            "Labour Party",
+            "Nazi Party",
+            "Ku-Klux-Klan",
+            "Democratic Party",
+            
+        ],
+
+        answer: 2,
+    },
+    {
+
+        question: " FFC stands for",
+    
+        options: [
+            "Foreign Finance Corporation",
+            "Film Finance Corporation",
+            "Federation of Football Council",
+            "None of the above",
+        ],
+        
+        answer: 2,
+
+    },
+    {
+        
+        question: " Fastest shorthand writer was",
+    
+        options: [
+            "Dr. G. D. Bist",
+            "J.R.D. Tata",
+            "J.M. Tagore",
+            "Khudada Khan",
+        ],
+        
+        answer: 1,
+    
+    },
+    {
+        
+        question: " Epsom (England) is the place associated with",
+    
+        options: [
+            "Horse racing",
+            "Polo",
+            "Shooting",
+            "Snooker",
+        ],
+        
+        answer: 1,
+    
+    },
+    {
+        
+        question: " First Afghan War took place in",
+    
+        options: [
+            "1839",
+            "1843",
+            "1833",
+            "1848",
+        ],
+        
+        answer: 1,
+    
+    },
+    
+    
+];
